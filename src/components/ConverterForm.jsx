dc90c188c504d2dee999fa68c64dd4956975f762
wilsonv244 <39635463+wilsonv244@users.noreply.github.com>
2025-01-15 22:47:07 -0500

    change Default Value

--- src/components/ConverterForm.jsx
+++ src/components/ConverterForm.jsx
@@ -4,7 +4,7 @@ import CurrencySelect from "./CurrencySelect";
 const ConverterForm = () => {
   const [amount, setAmount] = useState(100);
   const [fromCurrency, setFromCurrency] = useState("USD");
-  const [toCurrency, setToCurrency] = useState("INR");
+  const [toCurrency, setToCurrency] = useState("PEN");
   const [result, setResult] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
